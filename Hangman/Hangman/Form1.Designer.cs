78a79e975dc1fb0047cfedec5d8d8b7ab1dc44d2
Mscience1 <38621010+Mscience1@users.noreply.github.com>
2020-02-15 19:38:49 +0000

    Add files via upload

---
+++ Hangman/Hangman/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace Hangman
+{
+    partial class Hangman
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(139, 194);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 90);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Vs. Computer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(58, 159);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(109, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(55, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 40);
+            this.label1.TabIndex = 2;
+            this.label1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(58, 194);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Submit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "a",
+            "b",
+            "c",
+            "d",
+            "e",
+            "f",
+            "g",
+            "h",
+            "i",
+            "j",
+            "k",
+            "l",
+            "m",
+            "n",
+            "o",
+            "p",
+            "q",
+            "r",
+            "s",
+            "t",
+            "u",
+            "v",
+            "w",
+            "x",
+            "y",
+            "z"});
+            this.listBox1.Location = new System.Drawing.Point(1, 286);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 160);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.Visible = false;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(138, 286);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 160);
+            this.listBox2.TabIndex = 5;
+            this.listBox2.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Hangman.Properties.Resources.Hangman_0;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(568, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(220, 255);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(167, 270);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Used Letters";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 270);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Letters Not Yet Used";
+            this.label3.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(429, 194);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(197, 90);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Two Player";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(60, 194);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Enter Word";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(284, 89);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(197, 90);
+            this.button5.TabIndex = 11;
+            this.button5.Text = " Impossible Mode";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Hangman
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Hangman.Properties.Resources.pic;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Hangman";
+            this.Text = "Hangman";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Hangman/Hangman/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using Hangman.Properties;
+
+namespace Hangman
+{
+    public partial class Hangman : Form
+    {
+        Random rnd = new Random(); //random object 
+        int incorrect = 0; //number of incorrect tries
+        int correct = 0; //number of correct inputs
+        String word = ""; //word to be guessed 
+        Boolean impossible = false;
+
+        public Hangman()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Generate words, add choose random word
+            var words = Properties.Resources.words.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var words = Properties.Resources.words.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+
+            //setup game
+            pictureBox1.Visible = true;
+            button2.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            listBox1.Visible = true;
+            listBox2.Visible = true;
+            textBox1.Visible = true;
+            button1.Visible = false;
+            button3.Visible = false;
+            button5.Visible = false;
+
+            word = words.ElementAt(rnd.Next(0, words.Length - 1));
+            correct++;
+
+            //add word length to label 
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == ' ')
+                {
+                    label1.Text = label1.Text.Insert(i, " ");
+                }
+
+                //add star to indicate letter 
+                else
+                {
+                    label1.Text = label1.Text.Insert(i, "*");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //check if is a letter that was not used 
+            if (listBox1.Items.Contains(textBox1.Text))
+            {
+                //add to used column
+                listBox2.Items.Add(textBox1.Text);
+                listBox1.Items.Remove(textBox1.Text);
+            }
+
+            //If not print error message 
+            else
+            {
+                MessageBox.Show("Invaid Input");
+                incorrect--; //to not remove points for invalid input
+            }
+
+            //check if the letter is part of the selected word
+            if (word.Contains(textBox1.Text) && textBox1.Text.Length <= 1)
+            {
+                //Show the letters on the label 
+                for (int i = 0; i < word.Length; i++)
+                {
+                    //Show letters in word 
+                    if (Char.ToUpper(word[i]) == Char.ToUpper(textBox1.Text.ToCharArray()[0]) && impossible == false)
+                    {
+                        correct++; //add points
+                        label1.Text = label1.Text.Remove(i, 1);
+                        label1.Text = label1.Text.Insert(i, textBox1.Text);
+
+                        //Check if player has won 
+                        if (correct == word.Length)
+                        {
+                            MessageBox.Show("You Won!");
+                            Application.Restart();
+                        }
+
+                    }
+
+                    else if (impossible == true)
+                    {
+                        correct++;
+                
+                        //Check if player has won 
+                        if (correct == word.Length)
+                        {
+                            MessageBox.Show("You Won!");
+                            Application.Restart();
+                        }
+                    }
+                }
+            }
+
+            //if input is not in word 
+            else
+            {
+                incorrect++;
+            }
+            
+
+            //change image (not the best way to do it, but it works for small number of images)
+            if(incorrect == 1)
+            {
+                pictureBox1.Image = Resources.Hangman_2;
+            }
+
+            else if(incorrect == 2)
+            {
+                pictureBox1.Image = Resources.Hangman_3;
+            }
+
+            else if (incorrect == 3)
+            {
+                pictureBox1.Image = Resources.Hangman_4;
+            }
+
+            else if (incorrect == 4)
+            {
+                pictureBox1.Image = Resources.Hangman_5;
+            }
+
+            else if (incorrect == 5)
+            {
+                pictureBox1.Image = Resources.Hangman_6;
+            }
+
+            else if (incorrect == 6)
+            {
+                pictureBox1.Image = Resources.Hangman_7;
+                MessageBox.Show("You Lost!");
+                Application.Restart();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //setup game
+            pictureBox1.Visible = true;
+            button4.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            listBox1.Visible = true;
+            listBox2.Visible = true;
+            textBox1.Visible = true;
+            button1.Visible = false;
+            button3.Visible = false;
+            button5.Visible = false;
+        }
+
+        //Two Player Game
+        private void button4_Click(object sender, EventArgs e)
+        {
+            word = textBox1.Text;
+            button4.Visible = false;
+            button2.Visible = true;
+
+            //add word length to label 
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == ' ')
+                {
+                    label1.Text = label1.Text.Insert(i, " ");
+                }
+
+                //add star to indicate letter 
+                else
+                {
+                    label1.Text = label1.Text.Insert(i, "*");
+                }
+            }
+        }
+
+        //You can't see the word at all...good luck!
+        private void button5_Click(object sender, EventArgs e)
+        {
+            var words = Properties.Resources.words.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+
+            //setup game
+            pictureBox1.Visible = true;
+            button2.Visible = true;
+            textBox1.Visible = true;
+            button1.Visible = false;
+            button3.Visible = false;
+            button5.Visible = false;
+            word = words.ElementAt(rnd.Next(0, words.Length - 1));
+            correct++;
+
+            //add word length to label 
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == ' ')
+                {
+                    label1.Text = label1.Text.Insert(i, " ");
+                }
+
+                //add star to indicate letter 
+                else
+                {
+                    label1.Text = label1.Text.Insert(i, "*");
+                }
+            }
+        }
+    }
+ }
